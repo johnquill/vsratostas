c1f06a5c7016dcea41b28256d3522b03ab30e05b
johnquill <fedorhodyakov@gmail.com>
2022-10-31 11:05:55 +0300

    Добавлена тень к тексту, сглаживание

--- src/main/java/controllers/ImageEditor.java
+++ src/main/java/controllers/ImageEditor.java
@@ -8,34 +8,32 @@ import java.awt.font.TextAttribute;
 import java.awt.image.BufferedImage;
 import java.text.AttributedString;
 
-import static java.lang.Math.min;
 import static stasik.StasikBot.captions;
 
 public class ImageEditor {
 
     public static BufferedImage addCaption(BufferedImage image, PhotoSize photo) {
-        Graphics graphics = image.getGraphics();
         String caption = captions.getRandomCaption();
-        /*int fontSize = calcFontSize(photo.getWidth(), photo.getHeight(), caption.length());
-        int x = calcX(photo.getWidth(), caption.length(), fontSize);
-        int y = calcY(photo.getHeight(), fontSize);
-        graphics.setFont(new Font("Lobster Regular", Font.PLAIN, fontSize));
-        graphics.drawString(caption, x, y);*/
-
         Font font = createFontToFit(new Font("Lobster Regular", Font.PLAIN, 80), caption, image);
 
         AttributedString attributedText = new AttributedString(caption);
         attributedText.addAttribute(TextAttribute.FONT, font);
-        attributedText.addAttribute(TextAttribute.FOREGROUND, Color.WHITE);
 
-        Graphics g = image.getGraphics();
+        Graphics2D g2d = image.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
 
-        FontMetrics metrics = g.getFontMetrics(font);
+        FontMetrics metrics = g2d.getFontMetrics(font);
         int positionX = (image.getWidth() - metrics.stringWidth(caption)) / 2;
         int positionY = (int) ((image.getHeight() - metrics.getHeight() * 1.7) + metrics.getAscent());
 
-        g.drawString(attributedText.getIterator(), positionX, positionY);
+        attributedText.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
+        g2d.drawString(attributedText.getIterator(),
+                (int) (positionX + metrics.getHeight() * 0.03),
+                (int) (positionY + metrics.getHeight() * 0.03));
 
+        attributedText.addAttribute(TextAttribute.FOREGROUND, Color.WHITE);
+        g2d.drawString(attributedText.getIterator(), positionX, positionY);
         return image;
     }
 
@@ -60,18 +58,5 @@ public class ImageEditor {
             newFont = baseFont.deriveFont(baseFont.getStyle(), (float)newFontSize);
         }
         return newFont;
-    }
-
-    private static int calcX(Integer width, Integer captionLength, Integer fontSize) {
-        return (int) ((width - fontSize/2 * captionLength * (double) (4/3)) / 0.84 / 2) / 2;
-    }
-
-    private static int calcY(Integer height, Integer fontSize) {
-        return height - fontSize;
-    }
-
-    private static int calcFontSize(Integer width, Integer height, Integer captionLength) {
-        return min((int) ((width/captionLength/((double) 4/3))*2*0.84),
-                (int) (height/10/((double) 4/3)));
     }
 }
